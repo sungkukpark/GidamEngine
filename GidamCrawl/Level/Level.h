aa6a3c20916e8e4e2362a1b6ff9990e4e85f295b
Sungkuk Park <sungkukshawnpark@gmail.com>
2026-04-16 22:37:19 +0200

    add level class

---
+++ GidamCrawl/Level/Level.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+namespace Gidam
+{
+	// 모든 게임 레벨의 기반 클래스
+	// 게임에 배치되는 액터를 관리
+	// 액터에 프레임 이벤트를 전달하는 기능
+	// 액터에 필요한 부가 기능 제공
+	class Level
+	{
+	public:
+		Level();
+		virtual ~Level();
+	};
+}
